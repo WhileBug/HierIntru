3c4ab6d76b4eef1f77ea41cdc56c522db09a8cdb
WhileBug <whilebug@gmail.com>
2024-10-08 16:36:05 +0000

    train fl success

--- train_fl.py
+++ train_fl.py
@@ -7,7 +7,7 @@ import sys
 import csv, random
 from collections import defaultdict
 from numpy.random import dirichlet
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
 from torch import optim
 import torch.nn as nn
@@ -83,6 +83,19 @@ def create_fl_datasets(data_path, client_num, noniid_alpha):
     client_datasets = split_data_to_clients(datasets, client_num, noniid_alpha)
     return client_datasets
 
+def split_train_val(dataset, val_ratio=0.2):
+    """Split dataset into training and validation sets"""
+    train_len = int(len(dataset) * (1 - val_ratio))
+    val_len = len(dataset) - train_len
+    return random_split(dataset, [train_len, val_len])
+
+def aggregate_models(global_model, client_models):
+    """Aggregate client models into the global model"""
+    global_dict = global_model.state_dict()
+    for k in global_dict.keys():
+        global_dict[k] = torch.stack([client_model.state_dict()[k] for client_model in client_models], 0).mean(0)
+    global_model.load_state_dict(global_dict)
+
 def eval_net(net, loader, device, batch_size):
     """Evaluation model on test dataset"""
     net.eval()
@@ -109,59 +122,73 @@ def eval_net(net, loader, device, batch_size):
     net.train()
     return val_loss / (n_val * batch_size), val_acc / (n_val * batch_size)
 
-def train_net(net, client_datasets, device, epochs=5, batch_size=512, lr=0.0001, save_cp=True):
-    for client_idx, client_dataset in enumerate(client_datasets):
-        logging.info(f'Starting training for client {client_idx}')
-        train_loader = DataLoader(client_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
-        n_train = len(train_loader)
-        writer = SummaryWriter(comment=f'Client_{client_idx}_LR_{lr}_BS_{batch_size}')
-        global_step = 0
+def train_net(global_model, client_datasets, device, epochs=5, batch_size=512, lr=0.0001, save_cp=True):
+    global_test_dataset = BasicDataset(preprocess('dataset/test_data.csv'))
+    global_test_loader = DataLoader(global_test_dataset, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=True, drop_last=True)
 
-        optimizer = optim.RMSprop(net.parameters(), lr=lr, weight_decay=1e-8, momentum=0.9)
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.9, patience=20)
+    for epoch in range(epochs):
+        client_models = []
+        for client_idx, client_dataset in enumerate(client_datasets):
+            logging.info(f'Starting training for client {client_idx} in epoch {epoch + 1}')
+            train_dataset, val_dataset = split_train_val(client_dataset)
+            train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
+            val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=True)
+            
+            client_model = Net(input_dim=28, hidden_1=1024, hidden_2=512, hidden_3=256, out_dim=5).to(device)
+            client_model.load_state_dict(global_model.state_dict())
+            optimizer = optim.RMSprop(client_model.parameters(), lr=lr, weight_decay=1e-8, momentum=0.9)
+            scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.9, patience=20)
+            criterion = nn.CrossEntropyLoss()
+            writer = SummaryWriter(comment=f'Client_{client_idx}_Epoch_{epoch + 1}_LR_{lr}_BS_{batch_size}')
+            global_step = 0
 
-        criterion = nn.CrossEntropyLoss()
+            client_model.train()
+            for batch in train_loader:
+                data = batch['data']
+                true_labels = batch['label']
 
-        for epoch in range(epochs):
-            net.train()
+                data = data.to(device=device, dtype=torch.float32)
+                true_labels = true_labels.to(device=device, dtype=torch.float32)
 
-            epoch_loss = 0
-            with tqdm(total=n_train, desc=f'Client {client_idx} Epoch {epoch + 1}/{epochs}', unit=' record') as pbar:
-                for batch in train_loader:
-                    data = batch['data']
-                    true_labels = batch['label']
+                labels_pred = client_model(data)
+                loss = criterion(labels_pred, true_labels.long())
+                optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_value_(client_model.parameters(), 0.1)
+                optimizer.step()
+                
+                writer.add_scalar('Loss/train', loss.item(), global_step)
+                global_step += 1
 
-                    data = data.to(device=device, dtype=torch.float32)
-                    true_labels = true_labels.to(device=device, dtype=torch.float32)
+            val_loss, val_acc = eval_net(client_model, val_loader, device, batch_size)
+            logging.info(f'Client {client_idx} Validation loss: {val_loss}, Validation accuracy: {val_acc}')
+            writer.add_scalar('Loss/val', val_loss, global_step)
+            writer.add_scalar('Accuracy/val', val_acc, global_step)
+            scheduler.step(val_loss)
 
-                    labels_pred = net(data)
+            writer.close()
+            client_models.append(client_model)
 
-                    loss = criterion(labels_pred, true_labels.long())
+        # Aggregate client models into the global model
+        aggregate_models(global_model, client_models)
 
-                    epoch_loss += loss.item()
+        # Evaluate the global model on the global test dataset
+        global_test_loss, global_test_acc = eval_net(global_model, global_test_loader, device, batch_size)
+        logging.info(f'Global model test loss after epoch {epoch + 1}: {global_test_loss}, test accuracy: {global_test_acc}')
 
-                    writer.add_scalar('Loss/train', loss.item(), global_step)
+        # Evaluate each client model on the global test dataset
+        for client_idx, client_model in enumerate(client_models):
+            client_test_loss, client_test_acc = eval_net(client_model, global_test_loader, device, batch_size)
+            logging.info(f'Client {client_idx} model test loss after epoch {epoch + 1}: {client_test_loss}, test accuracy: {client_test_acc}')
 
-                    pbar.set_postfix(**{'loss (batch)': loss.item()})
-
-                    optimizer.zero_grad()
-                    loss.backward()
-                    nn.utils.clip_grad_value_(net.parameters(), 0.1)
-                    optimizer.step()
-
-                    pbar.update(data.shape[0])
-                    global_step += 1
-
-            if save_cp:
-                try:
-                    os.mkdir('checkpoints')
-                    logging.info('Created checkpoint directory')
-                except OSError:
-                    pass
-                torch.save(net.state_dict(), f'checkpoints/Client_{client_idx}_CP_epoch{epoch + 1}.pth')
-                logging.info(f'Client {client_idx} Checkpoint {epoch + 1} saved!')
-
-        writer.close()
+        if save_cp:
+            try:
+                os.mkdir('checkpoints')
+                logging.info('Created checkpoint directory')
+            except OSError:
+                pass
+            torch.save(global_model.state_dict(), f'checkpoints/Global_CP_epoch{epoch + 1}.pth')
+            logging.info(f'Global model checkpoint for epoch {epoch + 1} saved!')
 
 # Example usage
 data_path = 'dataset/train_data.csv'
@@ -175,7 +202,7 @@ if __name__ == '__main__':
     logging.info(f'Using device {device}')
 
     from net import Net
-    net = Net(input_dim=28, hidden_1=1024, hidden_2=512, hidden_3=256, out_dim=5)
-    net.to(device=device)
+    global_model = Net(input_dim=28, hidden_1=1024, hidden_2=512, hidden_3=256, out_dim=5)
+    global_model.to(device=device)
 
-    train_net(net=net, client_datasets=fl_datasets, epochs=5, batch_size=512, lr=0.0001, device=device)/
+    train_net(global_model=global_model, client_datasets=fl_datasets, epochs=5, batch_size=512, lr=0.0001, device=device)
